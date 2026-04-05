dfd00f40ad754a6a046f28f7737ece1700410a4a
Edward P. Tunod Jr. <etunodjr@smg.ph>
2026-04-05 20:58:28 +0800

    Fix Hive model extension and manifest injection

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,6 +1,6 @@
 import 'package:hive/hive.dart';
 
-class Expense {
+class Expense extends HiveObject {
   final String id;
   final double amount;
   final String category;
--- lib/models/utang.dart
+++ lib/models/utang.dart
@@ -1,6 +1,6 @@
 import 'package:hive/hive.dart';
 
-class Utang {
+class Utang extends HiveObject {
   final String id;
   final String personName;
   final double amount;
